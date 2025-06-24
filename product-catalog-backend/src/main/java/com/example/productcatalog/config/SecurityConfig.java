671aa5256cb0ee9c678df737684e0128e8bc61ec
Antun Franjin <antun.franjin@undabot.com>
2025-06-24 12:48:16 +0200

    fix: trying to fix auth

--- product-catalog-backend/src/main/java/com/example/productcatalog/config/SecurityConfig.java
+++ product-catalog-backend/src/main/java/com/example/productcatalog/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.example.productcatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,23 +14,37 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf().disable()
-        .headers(headers -> headers
-            .frameOptions(frameOptions -> frameOptions.sameOrigin())
-        )
-        .authorizeHttpRequests()
-        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-        .anyRequest().permitAll()
-        .and()
-        .httpBasic();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.sameOrigin())
+            )
+            .authorizeHttpRequests(auth -> auth
+                /* .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers(
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**"
+                ).permitAll() */
+                .anyRequest().permitAll()
+            )
+            .httpBasic();
 
-    return http.build();
-}
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        return new InMemoryUserDetailsManager(
+            User.withUsername("adminOGCS")
+                .password(passwordEncoder.encode("adminOGCS123"))
+                .roles("ADMIN")
+                .build()
+        );
     }
 }
